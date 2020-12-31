860c38bf2f5d71bbf5d340316a2f9b68f686f87c
Villajuan <solovillajuan@gmail.com>
2020-12-31 12:22:56 -0500

    Firts

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Login</title>
+</head>
+<body>
+    <div class="container_all">
+        <div class="ctn-form">
+            <img src="" alt="" class="logo">
+            <h1 class="tittle">Iniciar Sesion</h1>
+            <form action="">
+                <label for="">Email</label>
+                <input type="text">
+                <label for="">Contraseña</label>
+                <input type="password">
+                <input type="submit" value="Iniciar">
+            </form>
+            <span class="text-footer">No tienessssss una cuenta?<a href="#">Registrate</a></span>
+        </div>
+        <div class="ctn-tet">
+            <div class="capa">
+                <h1 class="title-description">
+                    Lorem ipsum dolor sit amet
+                </h1>
+                <p>Loremssss ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quos velss consequatur distinctio reprehenderit aut quia 
+                    incidunt error voluptatum. Voluptate rem sed quos tenetur
+                     sint voluptatum. Quis debitis obcaecati eaque voluptate.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
